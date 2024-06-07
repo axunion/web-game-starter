80b9a6a21a5b691c4bfbd850688eaa526357c9e6
axunion <120321953+axunion@users.noreply.github.com>
2024-06-07 23:46:19 +0900

    Include images of Devil Fruits

--- src/pages/devil-fruit-game/service/devilFruit.ts
+++ src/pages/devil-fruit-game/service/devilFruit.ts
@@ -11,13 +11,55 @@ type DevilFruitDefinition = {
 const creationLevel = 4;
 
 const definitions: DevilFruitDefinition[] = [
-  { level: 0, label: "fruit_0", radius: 10, fillStyle: "#ff0000", texture: "" },
-  { level: 1, label: "fruit_1", radius: 20, fillStyle: "#00ff00", texture: "" },
-  { level: 2, label: "fruit_2", radius: 30, fillStyle: "#0000ff", texture: "" },
-  { level: 3, label: "fruit_3", radius: 40, fillStyle: "#ffff00", texture: "" },
-  { level: 4, label: "fruit_4", radius: 50, fillStyle: "#ff00ff", texture: "" },
-  { level: 5, label: "fruit_5", radius: 60, fillStyle: "#00ffff", texture: "" },
-  { level: 6, label: "fruit_6", radius: 70, fillStyle: "#ffffff", texture: "" },
+  {
+    level: 0,
+    label: "fruit_0",
+    radius: 10,
+    fillStyle: "#ff0000",
+    texture: "image/fruit/barabara.png",
+  },
+  {
+    level: 1,
+    label: "fruit_1",
+    radius: 20,
+    fillStyle: "#00ff00",
+    texture: "image/fruit/utauta.png",
+  },
+  {
+    level: 2,
+    label: "fruit_2",
+    radius: 30,
+    fillStyle: "#0000ff",
+    texture: "image/fruit/hanahana.png",
+  },
+  {
+    level: 3,
+    label: "fruit_3",
+    radius: 40,
+    fillStyle: "#ffff00",
+    texture: "image/fruit/gorogoro.png",
+  },
+  {
+    level: 4,
+    label: "fruit_4",
+    radius: 50,
+    fillStyle: "#ff00ff",
+    texture: "image/fruit/meramera.png",
+  },
+  {
+    level: 5,
+    label: "fruit_5",
+    radius: 60,
+    fillStyle: "#00ffff",
+    texture: "image/fruit/yamiyami.png",
+  },
+  {
+    level: 6,
+    label: "fruit_6",
+    radius: 70,
+    fillStyle: "#ffffff",
+    texture: "image/fruit/gomugomu.png",
+  },
 ];
 
 export const createDevilFruit = (
@@ -34,7 +76,7 @@ export const createDevilFruit = (
       restitution: 0.5,
       friction: 0.1,
       render: {
-        fillStyle: definition.fillStyle,
+        // fillStyle: definition.fillStyle,
         sprite: {
           texture: definition.texture,
           xScale: (definition.radius * 2) / 100,
